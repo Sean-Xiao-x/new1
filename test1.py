c3a12d5536883635746a9197e988402f7577056c
Sean-Xiao-x <xhx495447923@gmail.com>
2016-02-02 12:47:02 +0800

    my second project

--- test1.py
+++ test1.py
@@ -3,5 +3,7 @@ def print_lo1(the_list):
     for each_item in the_list:
         if isinstance(each_item,list):
             print_lo1(each_item)
+        elif not isinstance(each_item, list):
+            print("haha")
         else:
             print(each_item)
